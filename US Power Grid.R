3314f1835b5204308b264848ea2fa3a95a083483
felixreiter <80854675+felixreiter@users.noreply.github.com>
2022-03-27 19:14:28 +0200

    Update US Power Grid.R

--- US Power Grid.R
+++ US Power Grid.R
@@ -19,36 +19,39 @@ deg
 # the following code prints out the highest degree, of a node, within the network
 max(degree(powerNet))
 
+#code that deletes random nodes number sample(<lowerbound>:order, <uppperbound>)
 list_delete <- sample(1:order, 1000)
 new_powerNet <- delete.vertices(powerNet, list_delete)
 gorder(new_powerNet)
 
+degcent <- centr_degree(powerNet, mode="all")
+max_degcent <- V(g)$name[degcent$res==max(degcent$res)]
+max_degcent
+
+evcent <- centr_eigen(powerNet)
+V(g)$name[evcent$res==max(evcent$res)]
+
+katzcent <- alpha_centrality(powerNet, alpha=0.2)
+V(g)$name[katcent$res==max(katcent$res)]
 
 
+prcent <- page_rank(powerNet, directed=FALSE)
+V(g)$name[prcent$res==max(prcent$res)]
 
 
-###algorithm idea to delete the node with the highest degree###
+closecent <- centr_clo(powerNet)
+V(g)$name[closecent$res==max(closecent$res)]
+
+
+betwcent <- centr_betw(powerNet, directed=FALSE)
+V(g)$name[betwcent$res==max(betwcent$res)]
 
 
 # This line prints out the name of the most central Node
 V(powerNet)$name[degree(powerNet)==max(degree(powerNet))]
 # select the name of the node, which degree is equal to the highest degree within the network, and print it out
 
-# while i kleiner 100
-#   setzte variable Nodemax auf die aktuelle node mit höchstem degree
-#     dann lösche diese 
 
-#repeat {
-  NodeWithCurrentHighestDegree <- V(powerNet)$name[degree(powerNet)==max(degree(powerNet))]
- g2 <- delete_vertices(powerNet, NodeWithCurrentHighestDegree)
- nwchd2 <- V(g2)$name[degree(g2)==max(degree(g2))]
-  #if(gorder(powerNet)==gorder(powerNet)-100){
-   # break
-  #}
-#}
-
-NodeWithCurrentHighestDegree
-nwchd2
 
 
 
