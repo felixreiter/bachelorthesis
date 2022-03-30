017beb66b6cf58b8e1b092d7197eaeb31f3fbb44
felixreiter <80854675+felixreiter@users.noreply.github.com>
2022-03-30 22:18:21 +0200

    Update Degree Plots.R

--- Degree Plots.R
+++ Degree Plots.R
@@ -1,20 +1,26 @@
 library(igraph)
 
 g <- make_empty_graph(n = 7, directed = FALSE)
-V(g)$name <- c("Alice","Bob","Anna","Alexa","Claire","John","Mike")
-g <- add_edges(g,c("Anna","Alice"))
-g <- add_edges(g,c("Anna","Alexa"))
-g <- add_edges(g,c("Alexa","Alice"))
-g <- add_edges(g,c("Alexa","Bob"))
-g <- add_edges(g,c("Alice","Claire"))
-g <- add_edges(g,c("Alice","John"))
-g <- add_edges(g,c("Alice","Bob"))
-g <- add_edges(g,c("Claire","Bob"))
-g <- add_edges(g,c("Claire","John"))
-g <- add_edges(g,c("Claire","Mike"))
+V(g)$name <- c("1","2","3","4","5","6","7")
+g <- add_edges(g,c("1","4"))
+g <- add_edges(g,c("1","6"))
+g <- add_edges(g,c("6","4"))
+g <- add_edges(g,c("6","z"))
+g <- add_edges(g,c("4","2"))
+g <- add_edges(g,c("4","3"))
+g <- add_edges(g,c("4","7"))
+g <- add_edges(g,c("2","7"))
+g <- add_edges(g,c("2","3"))
+g <- add_edges(g,c("2","5"))
+plot(g)
 
 #plot degree centrality
 degcent <- degree(g) 
 #node degrees sind so klein, das visualisieren unübersichtlich wäre, darum vergrößere ich sie um einen Faktor
 VS = 6 + 5*degcent
+plot(g, vertex.size=VS)
+
+#plot betweennes
+betwcent <-betweenness(g)
+VS = 14 + 5*betwcent
 plot(g, vertex.size=VS)
