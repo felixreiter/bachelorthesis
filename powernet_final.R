abd635c585817444501a15dc2901c72fd29e4d6f
felixreiter <80854675+felixreiter@users.noreply.github.com>
2022-04-24 17:56:52 +0200

    bug fixes

--- powernet_final.R
+++ powernet_final.R
@@ -1,14 +1,14 @@
 #large network, power grid of the USA
 library("igraph")
-library("tnet")
 
-g <- read.table("http://opsahl.co.uk/tnet/datasets/USpowergrid_n4941.txt")
+g <- read.table("power-US-Grid.mtx", header= TRUE, sep = " ",fill = TRUE)
 
-#@MISC{konect:2017:opsahl-powergrid,
-#title = {US power grid network dataset -- {KONECT}},
-#month = oct,
-#year = {2017},
-#url = {http://konect.cc/networks/opsahl-powergrid}
+#@inproceedings{nr,
+#  title={The Network Data Repository with Interactive Graph Analytics and Visualization},
+#  author={Ryan A. Rossi and Nesreen K. Ahmed},
+#  booktitle={AAAI},
+#  url={https://networkrepository.com},
+#  year={2015}
 #}
 
 g <- graph.data.frame(g, directed = FALSE)
@@ -24,7 +24,7 @@ components(g)
 mean_distance(g)
 diameter(g)
 
-##random deletion##
+  ##random deletion##
 
 #list of random nodes that will be deleted
 list_delete <- sample(1:order, n)
@@ -32,15 +32,15 @@ list_delete
 
 #delete the random nodes and save them in a new variable 
 random_delete <- delete.vertices(g, list_delete)
-#plot(random_delete)
 gorder(random_delete)
 max(degree(random_delete))
+mean(degree(random_delete))
 is_connected(random_delete)
 components(random_delete)
 mean_distance(random_delete)
 diameter(random_delete)
 
-##deletion of nodes with the highest degree centrality##
+  ##deletion of nodes with the highest degree centrality##
 
 degcent <- centr_degree(g, mode="all")
 
@@ -48,10 +48,10 @@ g2 <- g
 for (i in 1:n){
   g2 <- delete_vertices(g2,V(g2)$name[degcent$res==max(degcent$res)])
 }
-#plot(g2)
 highest_measure <-centr_degree(g2, mode = "all")
 V(g2)$name[highest_measure$res==max(highest_measure$res)]
 max(degree(g2))
+mean(degree(g2))
 is_connected(g2)
 components(g2)  
 mean_distance(g2)
@@ -65,15 +65,15 @@ g3 <- g
 for (i in 1:n){
   g3 <- delete_vertices(g3,V(g3)$name[evcent$vector==max(evcent$vector)])
 }
-#plot(g3)
 V(g3)$name[evcent$vector==max(evcent$vector)]
 is_connected(g3)
 max(degree(g3))
+mean(degree(g3))
 components(g3)
 mean_distance(g3)
 diameter(g3)
 
-##deletion of nodes with the highest Katz centrality##
+  ##deletion of nodes with the highest Katz centrality##
 
 katzcent <- alpha_centrality(g, alpha=0.2)
 
@@ -81,16 +81,17 @@ g4 <- g
 for (i in 1:n){
   g4 <- delete_vertices(g4,V(g4)$name[katzcent==max(katzcent)])
 }
-#plot(g4)
 gorder(g4)
 V(g4)$name[katzcent==max(katzcent)]
 max(degree(g4))
+mean(degree(g4))
 is_connected(g4)
 components(g4)
 mean_distance(g4)
 diameter(g4)
 
-##deletion of nodes with the highest PageRank centrality##
+  ##deletion of nodes with the highest PageRank centrality##
+
 prcent <- page_rank(g, directed=TRUE)
 
 g5 <- g
@@ -98,37 +99,41 @@ for (i in 1:n){
   g5 <- delete_vertices(g5,V(g5)$name[prcent$vector==max(prcent$vector)])
 }
 gorder(g5)
+max(degree(g5))
+mean(degree(g5))
 is_connected(g5)
 components(g5)
 max(degree(g5))
 mean_distance(g5)
 diameter(g5)
 
-##deletion of nodes with the highest closeness centrality##
+  ##deletion of nodes with the highest closeness centrality##
+
 closecent <- centr_clo(g)
 
 g6 <- g
 for (i in 1:n){
   g6 <- delete_vertices(g6,V(g6)$name[closecent$res==max(closecent$res)])
 }
-#plot(g6)
 V(g6)$name[closecent$res==max(closecent$res)]
 max(degree(g6))
+mean(degree(g6))
 is_connected(g6)
 components(g6)
 mean_distance(g6)
 diameter(g6)
 
-##deletion of nodes with the highest betweenness centrality##
+  ##deletion of nodes with the highest betweeness centrality##
+
 betwcent <- centr_betw(g, directed=TRUE)
 
 g7 <- g
 for (i in 1:n){
   g7 <- delete_vertices(g7,V(g7)$name[betwcent$res==max(betwcent$res)])
 }
-#plot(g7)
 V(g7)$name[betwcent$res==max(betwcent$res)]
 max(degree(g7))
+mean(degree(g7))
 is_connected(g7)
 components(g7)
 mean_distance(g7)
